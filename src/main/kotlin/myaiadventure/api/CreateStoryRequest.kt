2280621cbf91e2e123a867c1f0e3d69dcf56dfd1
ouhra <ouhrabkova.adel@gmail.com>
2026-09-08 21:05:59 +0200

    *

---
+++ src/main/kotlin/myaiadventure/api/CreateStoryRequest.kt
@@ -0,0 +1,17 @@
+package com.example.myaiadventure.api
+
+import com.example.myaiadventure.enums.Genre
+import com.example.myaiadventure.enums.MaturityLevel
+import com.example.myaiadventure.enums.WritingStyle
+import com.example.myaiadventure.exeptions.InvalidStoryRequestException
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CreateStoryRequest(
+    val title: String,
+    val genres: List<Genre>,
+    val tags: List<String>,
+    val maturityLevel: MaturityLevel,
+    val writingStyles: List<WritingStyle>,
+    val generalSetting: String
+)
---
+++ src/main/kotlin/myaiadventure/exceptions/InvalidStoryRequestException.kt
@@ -0,0 +1,3 @@
+package com.example.myaiadventure.exeptions
+
+class InvalidStoryRequestException(message: String) : Exception(message)
